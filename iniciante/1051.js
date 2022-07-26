588847013141079fa3607743777548ed5791581f
João Victor da S. Cirilo <jvscirilo12@gmail.com>
2022-07-26 14:28:28 -0300

    Solved!

--- iniciante/1051.js
+++ iniciante/1051.js
@@ -1,27 +1,24 @@
 // https://www.beecrowd.com.br/judge/pt/problems/view/1051
 
-lines = ["3000"];
-
-lines = parseFloat(lines[0]);
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+var renda = parseFloat(lines[0]);
 
 function calcularImposto(renda) {
     if (renda <= 2000) {
-        return "Isento";
-    } else {
-        if (renda <= 3000) {
-            return (renda - 2000) * 0.08;
-        } else {
-            if (renda <= 4500) {
-                return 1000 * 0.08 + (renda - 3000) * 0.18;
-            } else {
-                return 1000 * 0.08 + 1500 + (renda - 4500) * 0.18;
-            }
-        }
+        return null;
+    }
+    else if (renda <= 3000) {
+        return ((renda - 2000) * 0.08);
+    }
+    else if (renda <= 4500) {
+        return ((1000 * 0.08) + ((renda - 3000) * 0.18));
+    }
+    else {
+        return ((1000 * 0.08) + (1500 * 0.18) + ((renda - 4500) * 0.28));
     }
 }
 
-console.log(
-    calcularImposto(lines) === "Isento"
-        ? "Isento"
-        : `R$ ${calcularImposto(lines).toFixed(2)}`
-);
+const imposto = calcularImposto(renda);
+
+console.log((!imposto) ? 'Isento' : ('R$ ' + imposto.toFixed(2)));
